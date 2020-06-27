a21f2dfa4b654dd0b431a64db1412df167bb6c0a
gisnetz <gisnetz@msn.com>
2020-06-27 02:00:53 +0200

    Updates

---
+++ precache-manifest.170c561ad77e3b3e3a4560b6cf36286a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6e92e6966f93b557142043b2872aba5",
+    "url": "/movie-api/index.html"
+  },
+  {
+    "revision": "17089c39ad09716add58",
+    "url": "/movie-api/static/css/main.dd1e5ed4.chunk.css"
+  },
+  {
+    "revision": "fc69054c99d251d57769",
+    "url": "/movie-api/static/js/2.380c8b9c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-api/static/js/2.380c8b9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17089c39ad09716add58",
+    "url": "/movie-api/static/js/main.deb20312.chunk.js"
+  },
+  {
+    "revision": "0d786d6ba10ec48deb1d",
+    "url": "/movie-api/static/js/runtime-main.1eb54a45.js"
+  }
+]);
